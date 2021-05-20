76b13a35181ad5f559d60c12b82b8b45e79173b8
marshall chen <marshallchan666@outlook.com>
2021-05-21 04:31:22 +0800

    refactor the code for the create function of transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,42 +10,60 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.new(transaction_params)
     @transaction.wallet = @wallet
     authorize @transaction
+    @activity = @transaction.activity
 
-    if @transaction.activity == 'deposit'
-      @wallet.update(balance: @wallet.balance + @transaction.amount)
-      if @transaction.save
-        redirect_to wallet_path(@wallet), notice: 'Transaction was successfully created.'
-      else
-        render :new
-      end
-    elsif @transaction.activity == 'transfer'
-      if @transaction.amount <= @wallet.balance
-        @wallet.update(balance: @wallet.balance - @transaction.amount)
-        @receiver_wallet = User.find_by(email: @transaction.receiver_email).wallets.first
-        @receiver_wallet.update(balance: @receiver_wallet.balance + @transaction.amount)
-        Transaction.create(activity: "receive", amount: @transaction.amount, sender_email: @wallet.user.email, wallet: @receiver_wallet)
-        if @transaction.save
-          redirect_to wallet_path(@wallet), notice: 'Transaction was successfully created.'
-        else
-          render :new
-        end
-      else
-        flash.now[:alert] = "Transaction cannot be done due to insufficient cash."
-        render :new
-      end
+    case @activity
+    when 'deposit'
+      deposit_cash
+    when 'transfer'
+      transfer_cash
     else
-      if @transaction.amount <= @wallet.balance
-        @wallet.update(balance: @wallet.balance - @transaction.amount)
-        if @transaction.save
-          redirect_to wallet_path(@wallet), notice: 'Transaction was successfully created.'
-        else
-          render :new
-        end
-      else
-        flash.now[:alert] = "Transaction cannot be done due to insufficient cash."
-        render :new
-      end
+      withdraw_cash
     end
+  end
+
+  def deposit_cash
+    @wallet.update(balance: @wallet.balance + @transaction.amount)
+    transaction_save
+  end
+
+  def transfer_cash
+    if @transaction.amount <= @wallet.balance
+      @wallet.update(balance: @wallet.balance - @transaction.amount)
+      receiver_transaction_create
+      transaction_save
+    else
+      insufficient_cash_alert
+    end
+  end
+
+  def withdraw_cash
+    if @transaction.amount <= @wallet.balance
+      @wallet.update(balance: @wallet.balance - @transaction.amount)
+      transaction_save
+    else
+      insufficient_cash_alert
+    end
+  end
+
+  def receiver_transaction_create
+    @receiver_wallet = User.find_by(email: @transaction.receiver_email).wallets.first
+    @receiver_wallet.update(balance: @receiver_wallet.balance + @transaction.amount)
+    @sender_email = @wallet.user.email
+    Transaction.create(activity: "receive", amount: @transaction.amount, sender_email: @sender_email, wallet: @receiver_wallet)
+  end
+
+  def transaction_save
+    if @transaction.save
+      redirect_to wallet_path(@wallet), notice: 'Transaction was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def insufficient_cash_alert
+    flash.now[:alert] = "Transaction cannot be done due to insufficient cash."
+    render :new
   end
 
   private
